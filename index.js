bb88663afb8f38f0b430ae26b6d903c74b3d29e9
YongHyuk Seo <tjdydgur123@gmail.com>
2022-04-05 15:08:44 -0700

    add post api for registration page and add nodemon

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const { User } = require("./models/User");
+
+//application.x-www-form-urlencoded
+app.use(express.json());
+//will convert the user's input into the JSON format
+app.use(express.urlencoded({ extended: true }));
 
 // connect our express web to MongoDB
 const mongoose = require("mongoose");
@@ -10,6 +16,15 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, error });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 50,
+    maxlength: 50,
   },
   lastname: {
     type: String,
