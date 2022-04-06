7781abaa63a2dc3256f3ee034bf653006e7145a0
YongHyuk Seo <tjdydgur123@gmail.com>
2022-04-05 18:30:34 -0700

    hash the modified or new password using bcrypt library

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -31,6 +33,30 @@ const userSchema = new mongoose.Schema({
     // token expiration date
     type: Number,
   },
+});
+
+// hash the modified or new password before saving into database
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (user.isModified("password")) {
+    // generate a salt
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
